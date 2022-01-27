183557e3f14c90cf662c24f4b26e4f72c4c19345
kenchen879 <57173487+kenchen879@users.noreply.github.com>
2022-01-27 20:19:55 +0800

    Update version

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
